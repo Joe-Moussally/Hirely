27a8e304a5b6f491d3943db23e11cc74ad0043b8
MJoe111 <joemoussally111@gmail.com>
2022-06-27 15:02:03 +0300

    Removing Time To Live from token

--- laravel-server/app/Http/Controllers/JWTController.php
+++ laravel-server/app/Http/Controllers/JWTController.php
@@ -116,7 +116,7 @@ class JWTController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            // 'expires_in' => auth()->factory()->getTTL() * 60
         ]);
     }
 }
