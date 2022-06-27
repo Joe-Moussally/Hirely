4ad4796887b934fc0b1bcaea682ce44ff821b21d
MJoe111 <joemoussally111@gmail.com>
2022-06-27 17:53:48 +0300

    Adding addOffer api

--- laravel-server/app/Http/Controllers/OfferController.php
+++ laravel-server/app/Http/Controllers/OfferController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Offer;
+
+use Auth;
+
 class OfferController extends Controller
 {
-    //
+    
+    //api to add a Job Offer
+    public function addOffer(Request $Request) {
+
+        $offer = new Offer;
+        $offer->position = $Request->position;
+        $offer->description = $Request->description;
+        //getting authenticated user id
+        $offer->user_id = Auth::user()->id;
+        $offer->save();
+        
+
+        return response()->json([
+            'status' => 'success',
+            'offer' => $offer
+        ],200);
+    }
+
 }
--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -4,10 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JWTController;
 
+use App\Http\Controllers\OfferController;
+
+//JWT apis
 Route::group(['middleware' => 'api'], function($router) {
     Route::post('/register', [JWTController::class, 'register']);
     Route::post('/login', [JWTController::class, 'login']);
     Route::post('/logout', [JWTController::class, 'logout']);
     Route::post('/refresh', [JWTController::class, 'refresh']);
     Route::post('/profile', [JWTController::class, 'profile']);
+});
+
+//offer apis
+Route::group(['prefix' => 'offers'], function(){
+    Route::post('/add_offer',[OfferController::class, 'addOffer']);
 });
