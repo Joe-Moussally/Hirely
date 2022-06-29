25ca40bf3b6c79e3524ddaa339a675f1c7a9f4c5
MJoe111 <joemoussally111@gmail.com>
2022-06-29 18:20:18 +0300

    Adding login and signup routes to stack

--- react-native-app/navigations/newUserStack.js
+++ react-native-app/navigations/newUserStack.js
@@ -1,12 +1,25 @@
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Splash from '../screens/new-user-screens/splash';
+import SignUp from '../screens/new-user-screens/signup';
+import LogIn from '../screens/new-user-screens/signup';
+
 
 //screen functions to use in stack
 function SplashScreen() {
     return (<Splash />);
 }
+
+function SignUpScreen() {
+    return (<SignUp />);
+}
+
+function LogInScreen() {
+    return (<LogIn />);
+}
+
 
 //creating the stack navigator
 const Stack = createNativeStackNavigator();
@@ -16,7 +29,9 @@ export default function NewUserStack() {
         <NavigationContainer>
             <Stack.Navigator>
 
-                <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}}/>
+                <Stack.Screen name="Splash" component={SplashScreen} options={{headerShown: false}}/>
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
+                <Stack.Screen name="LogIn" component={LogInScreen} />
 
             </Stack.Navigator>
         </NavigationContainer>
---
+++ react-native-app/screens/new-user-screens/signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function SignUp() {
+    return (
+        <View>
+            <Text>SIGNUP</Text>
+        </View>
+    )
+}
