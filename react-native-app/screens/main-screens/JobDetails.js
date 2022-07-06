0cb7be955bcdbc76f7c070d541ac501e824e2479
MJoe111 <joemoussally111@gmail.com>
2022-07-06 20:40:46 +0300

    [Frontend] Adding header to job details

--- react-native-app/screens/main-screens/JobDetails.js
+++ react-native-app/screens/main-screens/JobDetails.js
@@ -1,11 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useEffect } from "react";
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 import axios from 'axios';
 import { localhost } from "../../globalVariables";
+import { globalStyles } from "../../styles/global";
 
 const JobDetails = ({route}) => {
     console.log('ROUTE PARAMS',route.params.id)//offer_id
+
+    const [details,setDetails] = useState([])
 
 
     useEffect(()=>{
@@ -20,13 +23,46 @@ const JobDetails = ({route}) => {
             url:'http://'+localhost+':8000/api/offers/'+route.params.id //offer id
         }).then((Response) => {
             console.log(' JOB DETAILS',Response.data)
+            setDetails(Response.data)
         }).catch((err)=>{
             console.log("ERROR JOB DETAILS")
         })
     },[])
     return ( 
-        <Text>JOB DETAILS</Text>
+        <View style={globalStyles.container}>
+
+            {/* Job Details Header */}
+            <View style={styles.header}>
+
+                {
+                    details.user['picture']?
+                    <Image source={details.user['details']}/>:
+                    <Image source={require('../../assets/profile/default_picture.jpg')} style={styles.picture}/>
+                }
+                <Text style={styles.name}>{details.user['name']}</Text>
+
+            </View>
+
+        </View>
      );
 }
  
 export default JobDetails;
+
+const styles = StyleSheet.create({
+    header:{
+        alignItems:'center'
+    },
+    picture:{
+        width:150,
+        height:150,
+        borderRadius:75,
+        borderWidth:2,
+        borderColor:'gray',
+        margin:25
+    },
+    name:{
+        fontSize:27,
+        fontWeight:'bold'
+    }
+})
