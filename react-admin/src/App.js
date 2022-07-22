58c0cb374ab7529a35926ddad9ea0d5b7a922904
MJoe111 <joemoussally111@gmail.com>
2022-07-22 21:34:48 +0300

    [Frontend] Linking Login admin to login api

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>TEST</h1>
+      
     </div>
   );
 }
--- react-admin/src/pages/Login.js
+++ react-admin/src/pages/Login.js
@@ -1,4 +1,27 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+import { localhost } from '../globalVariables';
+
 const Login = () => {
+
+    useEffect(()=>{
+        document.getElementById('login-btn').addEventListener('click',handleLogin)
+    },[])
+
+    const handleLogin = () => {
+        const data = new FormData()
+        //function to handle signup
+        data.append('email',document.getElementById('email').value)
+        data.append('password',document.getElementById('password').value)
+        axios({
+            method:'POST',
+            url:'http://'+localhost+':8000/api/login',
+            data:data
+        }).then(res => {
+            console.log(res.data)
+        })
+    }
+
     return (
         <>
             <div className="login-container">
@@ -10,9 +33,9 @@ const Login = () => {
                 <div className="form-container">
                     <img id='hero-logo' src={require('../assets/white-brand.png')}/>
                     <h1 id='form-title'>Admin Panel</h1>
-                    <input type='email' placeholder="Email" className='login-input'/>
-                    <input type='password' placeholder="Password" className='login-input'/>
-                    <button className="outline-button">Log In</button>
+                    <input type='email' placeholder="Email" className='login-input' id='email'/>
+                    <input type='password' placeholder="Password" className='login-input' id='password'/>
+                    <button className="outline-button" id='login-btn'>Log In</button>
                 </div>
 
             </div>
